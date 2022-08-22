b6d35d586c2e9fd1e9a04f6fc0be476dc6847fb5
andre-kr <33084669+andre-kr@users.noreply.github.com>
2022-08-22 10:20:40 +0200

    Basic Footer Layout
    Worked on the basic footer, currently hard coded the text instead of using the I18n string format.
    Not much exciting stuff here

--- corporate-site/src/layouts/Default.vue
+++ corporate-site/src/layouts/Default.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="layout">
     <header class="header">
-<!--        <strong>
+    <!--     <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong> -->
       <nav class="nav">
         <div class="navbar_top">
-          <div>
+          <div class="left">
             <g-link class="nav__link" to="/onlineShop/"> Online Shop</g-link>
             <g-link class="nav__link" to="/magazin/"> Magazin</g-link> 
           </div>
           <div class="lang_select">
             <locale-switcher></locale-switcher>
-            <!-- <g-link to="/magazin/"> DE</g-link>
-            <g-link to="/magazin/"> EN</g-link>
-            <g-link to="/magazin/"> FR</g-link> -->
           </div>
         </div>
         <!-- <g-link class="nav__link" to="/">Home</g-link>
@@ -22,6 +19,59 @@
       </nav>
     </header>
     <slot/>
+    <footer class="footer">
+        <div class="footer_top">
+          <g-image src="~/images/thinking_man.png" id="image_man"></g-image>
+          <p id="txt_questions">
+            <b>Sie haben noch Fragen?</b><br />
+            Wir sind gerne für Sie da und beantworten alle Ihre Fragen!
+          </p>
+          <button id="btn_contact">Zum Kontaktformular</button>
+        </div>
+        <div class="footer_center">
+          <div class="Company">
+              <dl>
+                  <dt><b>{{ $t('company') }}</b></dt>
+                  <dt>Unternehmensprofil</dt>
+                  <dt>Geschichte</dt>
+                  <dt>Unternehmenskultur</dt>
+                  <dt>Unternehmensstrategie</dt>
+                  <dt>Unsere Standorte</dt>
+                  <dt>Social Media</dt>
+                  <dt>Neuigkeiten</dt>
+              </dl>
+          </div>
+          <div class="Partner">
+              <dl>
+                  <dt><b>Partner</b></dt>
+                  <dt>Industriepartner</dt>
+                  <dt>Großhandelspartner</dt>
+              </dl>
+            
+          </div>
+          <div class="Career">
+              <dl>
+                  <dt><b>Karriere</b></dt>
+                  <dt>Überblick</dt>
+                  <dt>Job-Portal</dt>
+                  <dt>Arbeitsleben</dt>
+                  <dt>Kontakt</dt>
+              </dl>
+            
+          </div>
+          <div class="Bricofloor">
+              <dl>
+                  <dt><b>Unternehmen</b></dt>
+                  <dt>Unternehmensprofil</dt>
+                  <dt>Geschichte</dt>
+              </dl>
+            
+          </div>
+        </div>
+        <div class="footer_bottom">
+          <div>(c) 2022 BRICOFLOR GmbH</div>
+        </div>
+    </footer>
   </div>
 </template>
 
@@ -43,6 +93,45 @@ export default {
 </script>
 
 <style>
+
+.footer_bottom{
+  background-color: blue;
+  width: 100%;
+  height: 50px;
+  align-items: center;
+  display: flex;
+  justify-content: right;
+}
+
+.footer_top{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-content: center;
+  align-items: center;
+}
+
+.footer_center{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-content: center;
+}
+
+.footer{
+  position: relative;
+}
+
+#image_man {
+  width: 100px;
+  height: 100px;
+}
+
+#btn_contact {
+  height: 40px;
+  background-color: chocolate;
+}
+
 body {
   font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
   margin:0;
@@ -55,6 +144,10 @@ body {
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
+}
+
+.left {
+  display: inline-block;
 }
 
 .header {
@@ -99,6 +192,7 @@ body {
 .lang_select {
   position: relative;
   left: 30%;
+  float: right;
 }
 
 </style>
